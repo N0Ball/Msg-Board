41c24442248f336adbfe5ee98d63ceb3c8f6a5ca
N0Ball <n0ball@gmail.com>
2022-03-04 18:10:48 +0800

    Set up management system view

--- server.js
+++ server.js
@@ -1,15 +1,26 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const bodyparser = require('body-parser');
+const path = require('path');
 
 const app = express();
 
 dotenv.config({path:'.env'});
 const PORT = process.env.PORT || 8080
 
-app.unsubscribe(morgan('tiny'));
+app.use(morgan('tiny'));
 
-app.get('/', (req, res) => {
+app.use(bodyparser.urlencoded({ extended:true }));
+
+app.set("view engine", 'ejs');
+app.set("view", path.resolve(__dirname, "views/ejs"));
+
+app.use('/css', express.static(path.resolve(__dirname, "assets/css")));
+app.use('/img', express.static(path.resolve(__dirname, "assets/img")));
+app.use('/jg', express.static(path.resolve(__dirname, "assets/js")));
+
+app.get('/echo', (req, res) => {
     res.send("Hello, World!");
 })
 
